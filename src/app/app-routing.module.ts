2b55af2a7f423428d79b074c4d38c2581583ec55
Carlos Herrera <carlos.herrera@materiagris.pe>
2024-07-03 17:56:26 -0500

    use lazy load to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'admin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.component').then(m => m.AdminComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
